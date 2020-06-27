88701e233079a9cbc9a50d7927c3dff930ac5427
Luko Gjenero <luko@gopaktor.com>
2020-06-27 15:48:14 +0200

    added flame

---
+++ hack20/lib/game_screen.dart
@@ -0,0 +1,15 @@
+import 'package:flutter/material.dart';
+import 'game_time.dart';
+
+class GameScreen extends StatefulWidget {
+  const GameScreen({Key key}) : super(key: key);
+  static final _game = GameTime();
+
+  @override
+  _GameScreenState createState() => _GameScreenState();
+}
+
+class _GameScreenState extends State<GameScreen> {
+  @override
+  Widget build(BuildContext context) => GameScreen._game.widget;
+}
---
+++ hack20/lib/game_time.dart
@@ -0,0 +1,7 @@
+import 'dart:ui';
+import 'package:flame/game.dart';
+
+class GameTime extends Game {
+  void render(Canvas canvas) {}
+  void update(double t) {}
+}
