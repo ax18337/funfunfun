4b7d5a613228ef5d39eec847fccc91bd752590be
Luko Gjenero <luko@gopaktor.com>
2020-06-28 19:54:49 +0200

    finished intro

--- hack20/lib/components/intro.dart
+++ hack20/lib/components/intro.dart
@@ -1,8 +1,9 @@
+import 'dart:math';
 import 'dart:ui';
 
-import 'package:cloud_firestore/cloud_firestore.dart';
 import 'package:flutter/material.dart';
 import 'package:flutter/widgets.dart';
+import 'package:hack20/components/trash.dart';
 
 import '../game_time.dart';
 
@@ -44,6 +45,8 @@ class Intro {
   Paint _retroPaint;
   Paint _futurePaint;
   double time = 0;
+  List<Trash> _trash = List<Trash>();
+  Random _random = Random();
 
   Offset get center {
     return _rect.center;
@@ -55,6 +58,13 @@ class Intro {
 
   void update(double t) {
     time += t;
+
+    double progress = time / 20;
+    if (progress > 1.5) {
+      if (_random.nextDouble() <= 0.1) {
+        _addTrash();
+      }
+    }
   }
 
   void _drawScoreboard(Canvas c) {
@@ -73,22 +83,63 @@ class Intro {
     c.drawRect(_rect, _retroPaint);
     c.clipRect(_rect.inflate(-20));
 
-    c.translate(0, _rect.height * (1 - time / 20));
+    double progress = time / 20;
 
-    TextSpan span = TextSpan(
-        style: TextStyle(
-          color: WHITE,
-          fontSize: 24.0,
-          fontFamily: 'Joystix',
-        ),
-        text: _text);
-    TextPainter tp = TextPainter(
-        text: span,
-        textAlign: TextAlign.left,
-        textDirection: TextDirection.ltr);
-    tp.layout();
-    Rect box = _rect.inflate(-40);
-    tp.paint(c, box.topLeft);
+    if (progress > 1.5) {
+      TextSpan span = TextSpan(
+          style: TextStyle(
+            color: WHITE,
+            fontSize: 64.0,
+            fontFamily: 'Joystix',
+          ),
+          text: "GRAVITY BITES BACK");
+      TextPainter tp = TextPainter(
+          text: span,
+          textAlign: TextAlign.center,
+          textDirection: TextDirection.ltr);
+      tp.layout(maxWidth: _rect.width - 80);
+      tp.paint(c, Offset(_rect.left + 40, _rect.top + 40));
+
+      double y = _rect.top + 40 + tp.size.height + 20;
+
+      int step = (time * 2).floor();
+      if (step % 2 == 0) {
+        TextSpan span = TextSpan(
+            style: TextStyle(
+              color: WHITE,
+              fontSize: 24.0,
+              fontFamily: 'Joystix',
+            ),
+            text: "PRESS SPACE");
+        TextPainter tp = TextPainter(
+            text: span,
+            textAlign: TextAlign.left,
+            textDirection: TextDirection.ltr);
+        tp.layout();
+        tp.paint(c, Offset(_rect.center.dx - tp.width * 0.5, y));
+      }
+
+      for (var trash in _trash) {
+        trash.render(c);
+      }
+    } else {
+      c.translate(0, _rect.height * (1 - progress));
+
+      TextSpan span = TextSpan(
+          style: TextStyle(
+            color: WHITE,
+            fontSize: 24.0,
+            fontFamily: 'Joystix',
+          ),
+          text: _text);
+      TextPainter tp = TextPainter(
+          text: span,
+          textAlign: TextAlign.left,
+          textDirection: TextDirection.ltr);
+      Rect box = _rect.inflate(-40);
+      tp.layout(maxWidth: box.width);
+      tp.paint(c, box.topLeft);
+    }
 
     c.restore();
   }
@@ -103,4 +154,21 @@ but their calculations were wrong...
 and gravity has slowly been pulling the waste back to Earth causing catastrophic fiery re-entry showers burning the atmosphere.
 Now a team of brave space captains is putting their lives on the line to protect the Earth from these trash meteors.
 GRAVITY BITES BACK""";
+
+  void _addTrash() {
+    Rect rect = _rect.inflate(-40);
+
+    var dx = _random.nextDouble() * rect.width + rect.left;
+    var dy = rect.bottom - _random.nextDouble() * rect.height * 0.5;
+    var size = _random.nextDouble() * 80;
+
+    Trash star = Trash(
+      gameTime: gameTime,
+      center: Offset(dx, dy),
+      size: size,
+      speed: Offset(0, 0),
+    );
+
+    _trash.add(star);
+  }
 }
