21df047efd27d4a16fdd3e9027bd403fcd2b5eba
Luko Gjenero <luko@gopaktor.com>
2020-06-28 18:55:56 +0200

    highscore UI

--- hack20/lib/components/scoreboard.dart
+++ hack20/lib/components/scoreboard.dart
@@ -9,6 +9,12 @@ import '../game_time.dart';
 const WHITE = Color(0xffffffff);
 const GREEN = Color(0xff44ff66);
 
+class Entry {
+  Entry({@required this.name, this.score});
+  final String name;
+  final int score;
+}
+
 class Scoreboard {
   Scoreboard({@required this.gameTime}) {
     _rect = Rect.fromLTWH(
@@ -37,7 +43,7 @@ class Scoreboard {
   Rect _rect;
   Paint _retroPaint;
   Paint _futurePaint;
-  Map<String, int> _data;
+  List<Entry> _data;
 
   Offset get center {
     return _rect.center;
@@ -70,32 +76,176 @@ class Scoreboard {
 
     if (_data == null) {
       Firestore.instance.collection("scores").snapshots().listen((data) {
-        _data = Map<String, int>();
+        _data = List<Entry>();
         data.documents.forEach((element) {
-          debugPrint("user = ${element['user']} score = ${element['score']}");
-          _data[element['user']] = element['score'];
+          _data.add(Entry(name: element['user'], score: element['score']));
         });
+        _data.sort((a, b) => b.score.compareTo(a.score));
       });
-      var builder = ParagraphBuilder(ParagraphStyle(
-          textAlign: TextAlign.center, fontSize: 40, maxLines: 1))
-        ..addText("Loading high scores...");
-
-      var paragraph = builder.build()..layout(ParagraphConstraints(width: 500));
-      c.drawParagraph(paragraph, Offset(20, 20));
+      _drawRetroLoading(c);
     } else {
-      double space = 20;
-      _data.forEach((key, value) {
-        var builder = ParagraphBuilder(ParagraphStyle(
-            textAlign: TextAlign.center, fontSize: 24, maxLines: 1))
-          ..addText("$key --- $value");
-
-        var paragraph = builder.build()
-          ..layout(ParagraphConstraints(width: 500));
-        c.drawParagraph(paragraph, Offset(20, space));
-        space += 40;
-      });
+      _drawRetroLoaded(c);
     }
     c.restore();
+  }
+
+  void _drawRetroLoading(Canvas c) {
+    _drawRetroHeader(c);
+
+    TextSpan span = TextSpan(
+        style: TextStyle(
+          color: WHITE,
+          fontSize: 24.0,
+          fontFamily: 'Joystix',
+        ),
+        text: "LOADING ...");
+    TextPainter tp = TextPainter(
+        text: span,
+        textAlign: TextAlign.left,
+        textDirection: TextDirection.ltr);
+    tp.layout();
+    Rect box = Rect.fromLTWH(
+      _rect.center.dx - tp.size.width * 0.5,
+      _rect.center.dy - tp.size.height * 0.5,
+      tp.size.width,
+      tp.size.height,
+    );
+    tp.paint(c, box.topLeft);
+
+    _drawRetroFooter(c);
+  }
+
+  void _drawRetroLoaded(Canvas c) {
+    _drawRetroHeader(c);
+
+    double y = 100;
+
+    for (var i = 0; i < 20; i++) {
+      String line;
+      if (_data.length > i) {
+        Entry entry = _data[i];
+        if (entry.name.length >= 6) {
+          line = entry.name.substring(0, 6);
+        } else {
+          line = entry.name;
+          int missing = 6 - entry.name.length;
+          for (var i = 0; i < missing; i++) {
+            line += ".";
+          }
+        }
+      } else {
+        line = "......";
+      }
+
+      line += "......";
+
+      int score = 0;
+      if (_data.length > i) {
+        Entry entry = _data[i];
+        score = entry.score;
+      }
+
+      String scoreStr = score.toString();
+      if (scoreStr.length >= 8) {
+        line += scoreStr;
+      } else {
+        int missing = 8 - scoreStr.length;
+        for (var i = 0; i < missing; i++) {
+          line += ".";
+        }
+        line += scoreStr;
+      }
+
+      TextSpan span = TextSpan(
+          style: TextStyle(
+            color: WHITE,
+            fontSize: 24.0,
+            fontFamily: 'Joystix',
+          ),
+          text: line);
+      TextPainter tp = TextPainter(
+          text: span,
+          textAlign: TextAlign.left,
+          textDirection: TextDirection.ltr);
+      tp.layout();
+      Rect box = Rect.fromLTWH(
+        _rect.center.dx - tp.size.width * 0.5,
+        y,
+        tp.size.width,
+        tp.size.height,
+      );
+      tp.paint(c, box.topLeft);
+      y += 6 + tp.size.height;
+    }
+
+    _drawRetroFooter(c);
+  }
+
+  void _drawRetroHeader(Canvas c) {
+    double y = _rect.top + 20;
+    TextSpan span = TextSpan(
+        style: TextStyle(
+          color: WHITE,
+          fontSize: 32.0,
+          fontFamily: 'Joystix',
+        ),
+        text: "HIGHSCORES");
+    TextPainter tp = TextPainter(
+        text: span,
+        textAlign: TextAlign.left,
+        textDirection: TextDirection.ltr);
+    tp.layout();
+    Rect box = Rect.fromLTWH(
+      _rect.center.dx - tp.size.width * 0.5,
+      y,
+      tp.size.width,
+      tp.size.height,
+    );
+    tp.paint(c, box.topLeft);
+
+    y += tp.size.height;
+
+    span = TextSpan(
+        style: TextStyle(
+          color: WHITE,
+          fontSize: 32.0,
+          fontFamily: 'Joystix',
+        ),
+        text: "----------");
+    tp = TextPainter(
+        text: span,
+        textAlign: TextAlign.left,
+        textDirection: TextDirection.ltr);
+    tp.layout();
+    box = Rect.fromLTWH(
+      _rect.center.dx - tp.size.width * 0.5,
+      y,
+      tp.size.width,
+      tp.size.height,
+    );
+    tp.paint(c, box.topLeft);
+  }
+
+  void _drawRetroFooter(Canvas c) {
+    TextSpan span = TextSpan(
+        style: TextStyle(
+          color: WHITE,
+          fontSize: 18.0,
+          fontFamily: 'Joystix',
+        ),
+        text: "Copyright 2020");
+    TextPainter tp = TextPainter(
+        text: span,
+        textAlign: TextAlign.left,
+        textDirection: TextDirection.ltr);
+    tp.layout();
+    Rect box = Rect.fromLTWH(
+      _rect.center.dx - tp.size.width * 0.5,
+      _rect.bottom - tp.size.height - 20,
+      tp.size.width,
+      tp.size.height,
+    );
+    tp.paint(c, box.topLeft);
   }
 
   void _drawScoreboardFuture(Canvas c) {}
